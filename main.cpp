592dba8b9785f845d64b33ea7911caf31336840d
student <btanvir@uw.edu>
2025-05-02 00:59:02 -0700

    Started writing reduce fraction code

--- main.cpp
+++ main.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
 
-// added files to git repo
-int main() {
+void test();
 
+int main() {
+    test();
     return 0;
+}
+
+reduce(int a, int b)   // a/b
+
+
+void test() {
+    // reduce given fraction to minimum terms
+    // 2/4 -> 1/2
+    // 4/2 -> 2/1
+    // 6/8 -> 3/4
+    // 30/45 -> 2/3
+    // 6/6 -> 1/1
+    // 1/0 -> 0
+    // -6/8 -> 3/-4
 }
